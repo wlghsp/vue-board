f5f39517c924ea7ba4ec52e9ffb3a3eccc3c21f8
JIHO CHOI <wlghsp@gmail.com>
2024-08-29 13:33:13 +0900

    fix: age 필드 추가 및 생성자 수정

--- BoardApi/src/main/java/com/okestro/boardapi/dto/user/response/UserResponse.java
+++ BoardApi/src/main/java/com/okestro/boardapi/dto/user/response/UserResponse.java
@@ -10,10 +10,12 @@ public class UserResponse {
     private long id;
     private String userId;
     private String name;
+    private int age;
 
     public UserResponse(UserEntity user) {
         this.id = user.getId();
         this.userId = user.getUserId();
         this.name = user.getName();
+        this.age = user.getAge();
     }
 }
